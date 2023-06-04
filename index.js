b3299bce94033122c06afbe2ab1a6c18df1585e5
SCAN80 <scanavesse@yahoo.com.ar>
2023-06-04 18:09:13 -0300

    My second commit en Node

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ app.post('/api/notes/:id', (request, response)=> {
   response.status(201).json(newNote)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
